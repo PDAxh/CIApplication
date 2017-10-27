34df27ec306f9776773bb12d613e0719511c1518
Mattias <froggehxl@gmail.com>
2017-10-27 11:12:59 +0200

    dataGetter hämtar username+datum
    Från en git commit

--- public/javascripts/dataGetter.js
+++ public/javascripts/dataGetter.js
@@ -1,28 +1,35 @@
 const request = require('request');
 var fs = require('fs');
 
+var data;
+var author;
+var gitUserName;
+var dateTime;
+
+var gitProject = 'PDAxh/Jenkins';
+var commitIdentifier = 'b96d94c9176b4cbdae290ace6f1981b95d856a79';
+
+var fullLink = 'https://api.github.com/repos/' + gitProject + '/commits/' + commitIdentifier;
+// = https://api.github.com/repos/PDAxh/Jenkins/commits/b96d94c9176b4cbdae290ace6f1981b95d856a79
+
 var options = {
-    url: 'https://api.github.com/repos/PDAxh/Jenkins/commits/b96d94c9176b4cbdae290ace6f1981b95d856a79',
+    url: fullLink,
     headers: {
         'User-Agent': 'request'
     },
-    method: 'GET',
+    json: true
 
 };
 
-var data;
+request.get(options, function(error, response, body){
+    data = body;
+    author = body.commit.author.name;
+    gitUserName = body.author.login;
+    dateTime = body.commit.author.date;
 
-request(options, function(error, response, body){
-    if(error) console.log(error);
-    else  {
-        //console.log(JSON.parse(body));
-        data = JSON.parse(body);
-        console.log(data);
-    }
+    console.log('Author: ' + author);
+    console.log('Git username: ' + gitUserName);
+    console.log('Date & time: ' + dateTime);
 });
 
-
-
-
-
-
+console.log("The author is: " + author);
