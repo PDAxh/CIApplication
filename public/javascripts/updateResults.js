2a5b9276e63ca994d0a77bd8c04d73df4bb1a1bb
Unknown <joseflundstrom@gmail.com>
2017-10-30 15:32:18 +0100

    some changes

--- public/javascripts/updateResults.js
+++ public/javascripts/updateResults.js
@@ -1,8 +1,12 @@
-$(function(){
-    $("#mavenForm").hide();   /* Här läggs alla divar in, som hanterar de alternativ som är knutna till den typen av test som ska göras. Dessa divvar döljs för att sedan visas när de senare väljs. */
 
-    $("#dropDownJava").click(function(){
+$(document).ready(function () {
+    console.log("running");
+    $("#mavenForm").hide();
+    /* Här läggs alla divar in, som hanterar de alternativ som är knutna till den typen av test som ska göras. Dessa divvar döljs för att sedan visas när de senare väljs. */
+
+    $("#dropDownJava").click(function () {
         $("#chooseTest").hide();
         $("#mavenForm").show();
+
     });
 });
