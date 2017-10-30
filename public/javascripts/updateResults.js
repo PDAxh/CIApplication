39d03c9e1871aa6275f27e53fb025bc9b9d4cf61
Unknown <joseflundstrom@gmail.com>
2017-10-30 15:50:21 +0100

    Edit: The jQuery is correct but the .js file isn't running att all!

--- public/javascripts/updateResults.js
+++ public/javascripts/updateResults.js
@@ -1,12 +1,7 @@
-
-$(document).ready(function () {
-    console.log("running");
+$(function(){
     $("#mavenForm").hide();
-    /* Här läggs alla divar in, som hanterar de alternativ som är knutna till den typen av test som ska göras. Dessa divvar döljs för att sedan visas när de senare väljs. */
-
-    $("#dropDownJava").click(function () {
+    $("#dropDownJava").click(function(){
         $("#chooseTest").hide();
         $("#mavenForm").show();
-
     });
 });
