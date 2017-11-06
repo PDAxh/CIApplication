40dd740b1615bbbabe090aa9df51c4366f7a07ac
Lethol <joseflundstrom@gmail.com>
2017-11-06 11:11:03 +0100

    Bättre struktur på projektet!

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ var jobCreator = require('./public/javascripts/jobCreator');
 var dataGetter = require('./public/javascripts/dataGetter');
 var app = express();
 
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
